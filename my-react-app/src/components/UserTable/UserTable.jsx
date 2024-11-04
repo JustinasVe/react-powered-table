f6dddb5eb37e7891b316d3466e1823b6be53208b
unknown <justinasve@gmail.com>
2024-11-04 02:48:04 +0200

    feat: add sorting for each table column header

--- my-react-app/src/components/UserTable/UserTable.jsx
+++ my-react-app/src/components/UserTable/UserTable.jsx
@@ -3,6 +3,10 @@ import "./UserTable.css";
 
 export const UserTable = () => {
   const [users, setUsers] = useState([]);
+  const [sortParams, setSortParams] = useState({
+    columnKey: null,
+    direction: "asc",
+  });
 
   useEffect(() => {
     fetch("https://hiring-api.simbuka.workers.dev")
@@ -10,16 +14,56 @@ export const UserTable = () => {
       .then(setUsers);
   }, []);
 
+  // Function to handle sorting
+
+  const sortedUsers = () =>
+    [...users].sort((a, b) => {
+      if (sortParams.columnKey) {
+        const aValue = a[sortParams.columnKey];
+        const bValue = b[sortParams.columnKey];
+        if (aValue > bValue) {
+          return sortParams.direction === "asc" ? 1 : -1;
+        }
+        if (aValue < bValue) {
+          return sortParams.direction === "asc" ? -1 : 1;
+        }
+      }
+    });
+
+  // Function to request a sort for a particular column
+
+  const toggleSortOrder = (key) => {
+    let direction = "asc";
+    if (sortParams.columnKey === key && sortParams.direction === "asc") {
+      direction = "desc";
+    }
+    setSortParams({ columnKey: key, direction });
+  };
+
   return (
     <table>
       <thead>
         <tr>
-          <th>First name</th>
-          <th>Last name</th>
+          <th onClick={() => toggleSortOrder("firstName")}>
+            First name
+            {sortParams.columnKey === "firstName"
+              ? sortParams.direction === "asc"
+                ? " ↑"
+                : " ↓"
+              : ""}
+          </th>
+          <th onClick={() => toggleSortOrder("lastName")}>
+            Last name
+            {sortParams.columnKey === "lastName"
+              ? sortParams.direction === "asc"
+                ? " ↑"
+                : " ↓"
+              : ""}
+          </th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user, index) => (
+        {sortedUsers(users).map((user, index) => (
           <tr key={index}>
             <td>{user.firstName}</td>
             <td>{user.lastName}</td>
